bfeba1134d66e563f8d308ad8d72395229a715f0
Tumi <thorvaldur.tumi@gmail.com>
2021-12-10 18:12:59 +0000

    v2.1:
    build fyrir pages, prettier inn í script.js, folders

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 import { marked } from "marked";
 import * as readline from 'node:readline';
 import { stdin as input, stdout as output } from 'process';
 import * as fs from "node:fs";
+const prettier = require("prettier");
 
 let fasturHaus = `<!DOCTYPE html>
 <html lang="is">
@@ -13,7 +16,7 @@ let fasturHaus = `<!DOCTYPE html>
     <meta name="twitter:title" content="glósur">
     <meta name="twitter:description" content="svindlmiði fyrir próf, hvaða próf? veit ekki, stendur á síðunni">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="../styles/main.css">
     <title>hvadATitleAdVera</title>
 </head>
 <body>`;
@@ -31,15 +34,20 @@ let lokaOutput = "";
 const rl = readline.createInterface({ input, output });
 
 rl.question("Hvaða skrá á að breyta: ", function (file) {
+    addHtmlFile(file);
+    updateIndex(file);
+    rl.close();
+});
 
+function addHtmlFile(file) {
     // skráarnafnið nema mínus .md
-    let filename = file.substring(0,file.length-3)
-    fasturHaus = fasturHaus.replace("hvadATitleAdVera",filename)
+    let filename = file.substring(0,file.length-3);
+    fasturHaus = fasturHaus.replace("hvadATitleAdVera",filename);
 
     lokaOutput += fasturHaus;
 
     // aðeins að html-a markdownið meira
-    let skra = fs.readFileSync(file).toString();
+    let skra = fs.readFileSync("./markdown/" + file).toString();
     let m = marked.parse(skra);
     m = m.replace("<h1","<header><h1");
     m = m.replace("</h1>","</h1></header><main>");
@@ -64,13 +72,33 @@ rl.question("Hvaða skrá á að breyta: ", function (file) {
 
     lokaOutput += ass;
     lokaOutput += fasturFotur;
+    lokaOutput = prettier.format(lokaOutput,{parser: "html", tabWidth: 4});
 
     // skrifar út skrána í tilheyrandi html skrá
     let filenameHTML = filename + ".html";
-    fs.writeFile(filenameHTML, lokaOutput, function (err) {
+    fs.writeFile("./builds/"+filenameHTML, lokaOutput, function (err) {
         if (err) return console.log(err);
         console.log("Skrif tókust: " + file + " > " + filenameHTML);
       });
-    rl.close();
-});
 
+
+}
+
+function updateIndex(file) {
+    let filename = file.substring(0,file.length-3);
+    let filenameHTML = filename+ ".html"
+    let buildIndex = fs.readFileSync("./builds/index.html").toString();
+    
+    if (!buildIndex.includes(filename)) {
+        let BIMain = buildIndex.split("</ul>");
+        BIMain[0] += '<li><a href="./'+ filenameHTML + '">Glósur fyrir '+ filename +'</a></li>';
+        BIMain = BIMain.join('</ul>');
+        BIMain = prettier.format(BIMain,{parser: "html", tabWidth: 4});
+
+    
+        fs.writeFile("./builds/index.html", BIMain, function (err) {
+            if (err) return console.log(err);
+            console.log("Index.html uppfært!");
+        });
+    }
+}
